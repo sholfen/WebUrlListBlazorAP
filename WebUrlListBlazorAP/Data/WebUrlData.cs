a8f7407ce7e2c05dda18a3e7c21b4fe4309a00de
Peter Liang <peter@jxtech.io>
2023-06-17 19:59:44 +0800

    first upload

---
+++ WebUrlListBlazorAP/Data/WebUrlData.cs
@@ -0,0 +1,9 @@
+﻿namespace WebUrlListBlazorAP.Data
+{
+    public class WebUrlData
+    {
+        public string Name { get; set; } = string.Empty;
+        public string Url { get; set; } = string.Empty;
+        public DateTime Created { get; set; }
+    }
+}
---
+++ WebUrlListBlazorAP/Data/WebUrlDataService.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Concurrent;
+
+namespace WebUrlListBlazorAP.Data
+{
+    public class WebUrlDataService
+    {
+        private static ConcurrentBag<WebUrlData> _list = new ConcurrentBag<WebUrlData>();
+
+        public WebUrlDataService() 
+        {
+            //_list = new ConcurrentBag<WebUrlData>();
+        }
+
+        public void Insert(string name, string url)
+        {
+            WebUrlData data = new WebUrlData
+            {
+                Created = DateTime.Now,
+                Name = name,
+                Url = url
+            };
+            _list.Add(data);
+        }
+
+        public List<WebUrlData> GetList() 
+        {
+            return _list.ToList();
+        }
+    }
+}
